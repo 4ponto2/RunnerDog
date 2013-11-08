59f56bcab294cd63214d9cb2b183ba0e5a54ad18
giovannecaires <giovannecaires@uol.com.br>
2013-11-08 17:24:43 -0200

    08/11

--- Assets/Scripts/Runner/CollisionCoin.js
+++ Assets/Scripts/Runner/CollisionCoin.js
@@ -1,6 +1,18 @@
 #pragma strict
 
 static var coins = 0;
+private var timesec : float;
+//private var rot : float;
+
+function Update() {
+
+	timesec = Time.time%60;
+//	rot = (timesec*300)/60;
+	
+	this.transform.rotation = Quaternion.Euler(0,(timesec*360),0);
+	
+
+}
 
 function OnTriggerEnter(collision:Collider){
 
